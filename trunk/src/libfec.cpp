e258e7bbb3efae246a7e9827cae9eb4741e4418d
lim_maddin@gmx.net <lim_maddin@gmx.net@5227e1e4-b8c2-11de-b528-055f0b42e881>
2009-10-15 07:45:35 +0000

    see change.log

--- trunk/src/libfec.cpp
+++ trunk/src/libfec.cpp
@@ -16,3 +16,19 @@
 //      along with this program; if not, write to the Free Software
 //      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 //      MA 02110-1301, USA.
+
+
+#include "libfec.hpp"
+
+libfce::libfce(){
+};
+
+
+libfce::~libfce(){
+};
+
+
+void libfce::Initialize(){
+	
+}
+
