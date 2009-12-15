1cfec669988bf4f1fd8cb2c80c06fa0c3bdb5d41
lim_maddin@gmx.net <lim_maddin@gmx.net@5227e1e4-b8c2-11de-b528-055f0b42e881>
2009-12-15 07:07:27 +0000

    Header changend to blog

--- trunk/src/algorithm.cpp
+++ trunk/src/algorithm.cpp
@@ -1,4 +1,4 @@
-//		algorithm.cpp
+/*		algorithm.cpp
 //
 //		Copyright by hofmann.martin@gmx.de
 //
@@ -15,7 +15,7 @@
 //      You should have received a copy of the GNU General Public License
 //      along with this program; if not, write to the Free Software
 //      Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
-//      MA 02110-1301, USA.
+//      MA 02110-1301, USA.*/
 
 #include <iostream>
 #include "algorithm.hpp"
@@ -23,6 +23,11 @@
 
 
 algorithm::algorithm(){
+	//fill m_box with content from static const keys[]
+	for( int i = 0; i < 26; i++ ){
+		m_box[i] = keys[i];
+		std::cout << m_box[i] << std::endl;
+	}
 }//algorithm
 
 
@@ -31,16 +36,8 @@ algorithm::~algorithm(){
 
 
 int algorithm::AlgorithmInitialize( int password_length ){
-	//fill m_box with content from static const keys[]
-	for( int i = 0; i < 26; i++ ){
-		m_box[i] = keys[i];
-		std::cout << m_box[i] << std::endl;
-	}
-	
-	//get the numbers of which keys we use to generate the password
-	/*{
-		 * ToDo: Eine rechnung aufstellen um zu berechnen welche keys benutzt werden sollen
-	 }*/
+	 //ToDo: Eine rechnung aufstellen um zu berechnen welche keys benutzt werden sollen
+	 
 	int useever = ( password_length % 26 );
 	
 	return (0x00);
